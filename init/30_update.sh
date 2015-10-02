e99c05145014e6bcb6cef053d52097af7b05493f
sparklyballs <sparklyballs@gmail.com>
2015-10-02 09:07:56 +0100

    change to update git init, trying another way

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ ! -d /app/sickrage/.git ]; then
-git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage
-else
+[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SiCKRAGETV/SickRage.git /app/sickrage && chown -R abc:abc /app /config && exit 0
 cd /app/sickrage
 git pull
-fi
-chown -R abc:abc /app /config
+chown -R abc:abc /app
