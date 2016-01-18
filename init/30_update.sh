01dac8f492c8f4df0da9eab5d8c8697320cfb2d7
sparklyballs <sparklyballs@gmail.com>
2016-01-18 09:31:55 +0000

    add opt out for autoupdates to git update init

--- init/30_update.sh
+++ init/30_update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-[[ ! -d /app/sickrage/.git ]] && git clone https://github.com/SickRage/SickRage.git /app/sickrage
+[[ ! -d /app/sickrage/.git ]] && (git clone https://github.com/SickRage/SickRage.git /app/sickrage && \
+chown -R abc:abc /app /config)
+
+# opt out for autoupdates
+[ "$ADVANCED_DISABLEUPDATES" ] && exit 0
+
 cd /app/sickrage
 git pull
 chown -R abc:abc /app /config
